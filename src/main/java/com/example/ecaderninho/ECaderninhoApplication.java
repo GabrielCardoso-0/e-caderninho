4681976c2a448ea6a1d5d58e5069390fe7f01bbe
bielcount <gabrielcardoso0623@gmail.com>
2026-01-17 22:45:57 -0300

    add: adicionar packages dos modulos

---
+++ src/main/java/com/example/ecaderninho/ECaderninhoApplication.java
@@ -0,0 +1,12 @@
+package com.example.ecaderninho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECaderninhoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECaderninhoApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/ecaderninho/ECaderninhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecaderninho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECaderninhoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
